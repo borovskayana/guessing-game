ecbb8bca1a281a78bfde9d2f1e1d85838370095b
borovskayana <109426911+borovskayana@users.noreply.github.com>
2023-02-27 15:16:07 +0100

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    };
 
-    }
+
 
     guess() {
+        this.middle = Math.ceil((this.max + this.min)/2);
+        return this.middle;
+    };
 
-    }
+
 
     lower() {
+        this.max = this.middle;
+    };
 
-    }
+
 
     greater() {
+        this.min = this.middle;
+    };
 
-    }
+
+
 }
 
 module.exports = GuessingGame;
